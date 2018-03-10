c79e7cda0e4e397de02c34fe59208d1320d39822
Satyaki Ghosh <satyakig901@hotmail.com>
2018-03-10 00:05:59 -0700

    a

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        Imdb_Omdb.makeIDS();
+        long start = System.currentTimeMillis();
 
+        Imdb_Omdb.makeIDS();
         Exit one = new Exit(true);
         Thread t1 = new Thread(one);
         t1.setDaemon(true);
         t1.start();
-
         Imdb_Omdb.doPosts();
 
 
-
-
         Videos.readIDs();
-
         Exit two = new Exit(false);
         Thread t2 = new Thread(two);
         t2.setDaemon(true);
         t2.start();
-
         Videos.makeJson();
+
+        long end = System.currentTimeMillis();
+        long diff = end - start;
+        System.out.println("\nTime elapsed = " + (diff / (60 * 1000) % 60) +  "mins  " + diff / 1000 % 60 + " secs");
     }
 }
